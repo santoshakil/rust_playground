8a126d50f5de549a47eefa7321f71ff89c610a8d
Santo Shakil <shakil.mezbah@gmail.com>
2022-12-31 18:26:23 +0600

    types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
-    println!("Hello, world!");
+    // Scalar types
+    let x: i32 = 5; // 32-bit signed integer
+    let y: f32 = 3.14; // 32-bit floating point
+    let z: bool = true; // Boolean
+    let c: char = 'a'; // Unicode character
+
+    // Compound types
+    let tuple: (i32, f32, char) = (5, 3.14, 'a');
+    let array: [i32; 3] = [1, 2, 3];
+    let slice: &[i32] = &[1, 2, 3];
+    let string: String = "hello".to_string();
+
+    let person: Person = Person {
+        name: "Alice".to_string(),
+        age: 30,
+    };
+
+    println!("x is {}", x);
+    println!("y is {}", y);
+    println!("z is {}", z);
+    println!("c is {}", c);
+
+    println!("tuple is {:?}", tuple);
+    println!("array is {:?}", array);
+    println!("slice is {:?}", slice);
+    println!("string is {}", string);
+
+    println!("{} is {} years old", person.name, person.age);
+}
+
+struct Person {
+    name: String,
+    age: u8,
 }
