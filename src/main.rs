526470958265b537f17e86a3423720c43aa83f8b
Santo Shakil <shakil.mezbah@gmail.com>
2022-12-31 20:08:08 +0600

    ownership and borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // Declare a variable `s` of type `String` and give it ownership of a new String value.
+    let mut s = String::from("hello");
+
+    // Borrow `s` immutably and pass it to the `calculate_length` function.
+    // The value of `s` cannot be modified inside the `calculate_length` function.
+    let len = calculate_length(&s);
+    println!("The length of '{}' is {}.", s, len);
+
+    // Borrow `s` mutably and pass it to the `change` function.
+    // The value of `s` can be modified inside the `change` function.
+    change(&mut s);
+    println!("{}", s);
+}
+
+// This function immutably borrows a `String` value and returns its length.
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+// This function mutably borrows a `String` value and appends ", world" to it.
+fn change(s: &mut String) {
+    s.push_str(", world");
 }
