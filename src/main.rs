1fb401e5fe56294031411c8587fb650f94fda1f9
Santo Shakil <shakil.mezbah@gmail.com>
2023-06-13 15:38:17 +0600

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,40 @@
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
+use std::thread;
+
 fn main() {
-    // Declare a variable `s` of type `String` and give it ownership of a new String value.
-    let mut s = String::from("hello");
+    // Example with a normal bool
+    let normal_bool = false;
+    let normal_bool_arc = Arc::new(Mutex::new(normal_bool));
 
-    // Borrow `s` immutably and pass it to the `calculate_length` function.
-    // The value of `s` cannot be modified inside the `calculate_length` function.
-    let len = calculate_length(&s);
-    println!("The length of '{}' is {}.", s, len);
+    let normal_bool_thread = thread::spawn(move || {
+        for _ in 0..10 {
+            let mut normal_bool = normal_bool_arc.lock().unwrap();
+            if !*normal_bool {
+                println!("Normal bool is false. Updating to true.");
+                *normal_bool = true;
+            } else {
+                println!("Normal bool is true. Skipping update.");
+            }
+            thread::sleep(std::time::Duration::from_millis(100));
+        }
+    });
+    normal_bool_thread.join().unwrap();
 
-    // Borrow `s` mutably and pass it to the `change` function.
-    // The value of `s` can be modified inside the `change` function.
-    change(&mut s);
-    println!("{}", s);
-}
+    // Example with an AtomicBool
+    let atomic_bool = AtomicBool::new(false);
+    let atomic_bool_arc = Arc::new(atomic_bool);
 
-// This function immutably borrows a `String` value and returns its length.
-fn calculate_length(s: &String) -> usize {
-    s.len()
-}
-
-// This function mutably borrows a `String` value and appends ", world" to it.
-fn change(s: &mut String) {
-    s.push_str(", world");
+    let atomic_bool_thread = thread::spawn(move || {
+        for _ in 0..10 {
+            if !atomic_bool_arc.load(Ordering::SeqCst) {
+                println!("AtomicBool is false. Updating to true.");
+                atomic_bool_arc.store(true, Ordering::SeqCst);
+            } else {
+                println!("AtomicBool is true. Skipping update.");
+            }
+            thread::sleep(std::time::Duration::from_millis(100));
+        }
+    });
+    atomic_bool_thread.join().unwrap();
 }
