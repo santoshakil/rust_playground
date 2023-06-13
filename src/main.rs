3e1d8fe5d0f0a4ded5ec83d04b15f7fa2b325509
Santo Shakil <shakil.mezbah@gmail.com>
2023-06-13 16:31:45 +0600

    common

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,40 @@
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::{Arc, Mutex};
-use std::thread;
-
 fn main() {
-    // Example with a normal bool
-    let normal_bool = false;
-    let normal_bool_arc = Arc::new(Mutex::new(normal_bool));
+    find_common_last();
+    find_common_first();
+}
 
-    let normal_bool_thread = thread::spawn(move || {
-        for _ in 0..10 {
-            let mut normal_bool = normal_bool_arc.lock().unwrap();
-            if !*normal_bool {
-                println!("Normal bool is false. Updating to true.");
-                *normal_bool = true;
-            } else {
-                println!("Normal bool is true. Skipping update.");
-            }
-            thread::sleep(std::time::Duration::from_millis(100));
+fn find_common_first() {
+    let s1 = "x/b/a/b/c.txt";
+    let s2 = "x/b/a/x/b/c.txt";
+
+    let mut common = String::new();
+
+    for (_, (c1, c2)) in s1.chars().zip(s2.chars()).enumerate() {
+        if c1 == c2 {
+            common.push(c1);
+        } else {
+            break;
         }
-    });
-    normal_bool_thread.join().unwrap();
+    }
 
-    println!("............................................");
-    println!("............................................");
+    println!("Common first part: {}", common);
+}
 
-    // Example with an AtomicBool
-    let atomic_bool = AtomicBool::new(false);
-    let atomic_bool_arc = Arc::new(atomic_bool);
+fn find_common_last() {
+    let s1 = "x/b/a/b/c.txt";
+    let s2 = "x/b/a/x/b/c.txt";
 
-    let atomic_bool_thread = thread::spawn(move || {
-        for _ in 0..10 {
-            if !atomic_bool_arc.load(Ordering::SeqCst) {
-                println!("AtomicBool is false. Updating to true.");
-                atomic_bool_arc.store(true, Ordering::SeqCst);
-            } else {
-                println!("AtomicBool is true. Skipping update.");
-            }
-            thread::sleep(std::time::Duration::from_millis(100));
+    let mut common = String::new();
+
+    for (_, (c1, c2)) in s1.chars().rev().zip(s2.chars().rev()).enumerate() {
+        if c1 == c2 {
+            common.push(c1);
+        } else {
+            break;
         }
-    });
-    atomic_bool_thread.join().unwrap();
+    }
+
+    common = common.chars().rev().collect::<String>();
+
+    println!("Common last part: {}", common);
 }
