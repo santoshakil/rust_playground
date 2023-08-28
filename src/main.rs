780e161a06e1328422460dfb84a682fd3cd5352c
Santo Shakil <shakil.mezbah@gmail.com>
2023-08-28 19:26:30 +0600

    crossbea,

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let (s, r) = crossbeam_channel::unbounded::<String>();
+
+    tokio::spawn(async move {
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        s.send("Hello world!".to_string()).unwrap();
+    });
+
+    loop {
+        if let Ok(msg) = r.try_recv() {
+            println!("{}", msg);
+            break;
+        }
+    }
 }
