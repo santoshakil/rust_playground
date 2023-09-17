5841e1c42787db3f0507f162483a3ecb54a9c379
Santo Shakil <shakil.mezbah@gmail.com>
2023-09-17 18:52:00 +0600

    match

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-use tokio::task;
-
 #[tokio::main]
 async fn main() {
-    let x = "Hello, world!";
-    for _ in 0..5 {
-        _ = task::spawn(async move {
-            println!("Async start!");
-            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-            println!("Async end! - {}", x);
-        });
+    match 5.5 {
+        n if is_even(n) => println!("z is Even"),
+        n if is_odd(n) => println!("z is Odd"),
+        n if is_number(n) => println!("z is a Number"),
+        _ => println!("z is something else"),
     }
-    tokio::time::sleep(std::time::Duration::from_secs(20)).await;
-    println!("Done! - {}", x);
+}
+
+fn is_even(n: f32) -> bool {
+    println!("is_even called");
+    n % 2.0 == 0.0
+}
+
+fn is_odd(n: f32) -> bool {
+    println!("is_odd called");
+    n % 2.0 != 0.0
+}
+
+fn is_number(n: f32) -> bool {
+    println!("is_number called");
+    n.is_finite()
 }
