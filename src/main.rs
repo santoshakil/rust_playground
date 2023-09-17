b5755881ffacd603819640a2b27a826237d05ea1
Santo Shakil <shakil.mezbah@gmail.com>
2023-09-17 18:39:10 +0600

    async move

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::task;
+
+#[tokio::main]
+async fn main() {
+    let x = "Hello, world!";
+    for _ in 0..5 {
+        _ = task::spawn(async move {
+            println!("Async start!");
+            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+            println!("Async end! - {}", x);
+        });
+    }
+    tokio::time::sleep(std::time::Duration::from_secs(20)).await;
+    println!("Done! - {}", x);
 }
