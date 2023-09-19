cd71d086ed21696d10a0ae817a5e8a64ddf16d55
Santo Shakil <shakil.mezbah@gmail.com>
2023-09-19 20:09:15 +0600

    rwlock

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
-#[tokio::main]
-async fn main() {
-    match 5.5 {
-        n if is_even(n) => println!("z is Even"),
-        n if is_odd(n) => println!("z is Odd"),
-        n if is_number(n) => println!("z is a Number"),
-        _ => println!("z is something else"),
+use std::sync::{Arc, RwLock};
+use std::thread;
+
+fn main() {
+    let data = Arc::new(RwLock::new(Vec::new()));
+
+    let handles: Vec<_> = (0..4)
+        .map(|i| {
+            let data = Arc::clone(&data);
+            thread::spawn(move || {
+                let mut data = data.write().unwrap();
+                data.push(i);
+            })
+        })
+        .collect();
+
+    for handle in handles {
+        handle.join().unwrap();
     }
-}
 
-fn is_even(n: f32) -> bool {
-    println!("is_even called");
-    n % 2.0 == 0.0
-}
-
-fn is_odd(n: f32) -> bool {
-    println!("is_odd called");
-    n % 2.0 != 0.0
-}
-
-fn is_number(n: f32) -> bool {
-    println!("is_number called");
-    n.is_finite()
+    let final_data = data.read().unwrap();
+    println!("Final Vec: {:?}", *final_data);
 }
