4a3c0f62f98efc6fe100d5210434eb800044ad4d
Santo Shakil <shakil.mezbah@gmail.com>
2023-10-08 09:17:59 +0600

    notification

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,10 @@
-use std::sync::{Arc, RwLock};
-use std::thread;
+use notify_rust::Notification;
 
 fn main() {
-    let data = Arc::new(RwLock::new(Vec::new()));
-
-    let handles: Vec<_> = (0..4)
-        .map(|i| {
-            let data = Arc::clone(&data);
-            thread::spawn(move || {
-                let mut data = data.write().unwrap();
-                data.push(i);
-            })
-        })
-        .collect();
-
-    for handle in handles {
-        handle.join().unwrap();
-    }
-
-    let final_data = data.read().unwrap();
-    println!("Final Vec: {:?}", *final_data);
+    Notification::new()
+        .summary("Firefox News")
+        .body("This will almost look like a real firefox notification.")
+        .icon("firefox")
+        .show()
+        .unwrap();
 }
