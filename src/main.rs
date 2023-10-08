b32e90edf4dc854c318c2efbaba831870ca320e4
Santo Shakil <shakil.mezbah@gmail.com>
2023-10-08 17:54:19 +0600

    mutable oncelock

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ static ONCE: OnceLock<RwLock<i64>> = OnceLock::new();
 
 fn main() {
     let lock = ONCE.get_or_init(|| RwLock::new(0));
-    let mut value = lock.write().unwrap();
-    *value = 1;
-    println!("Value: {}", *value);
+    {
+        let mut value = lock.write().unwrap();
+        *value = 1;
+    }
+    println!("Value: {}", lock.read().unwrap());
 }
