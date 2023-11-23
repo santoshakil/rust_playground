f0dd1abffa749dd7c073330491e62b93dad8b473
Santo Shakil <shakil.mezbah@gmail.com>
2023-11-23 22:30:19 +0600

    file size limit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use log::{debug, error, info, trace, warn, LevelFilter, SetLoggerError};
 use log4rs::{
-    append::{console::ConsoleAppender, file::FileAppender},
+    append::{
+        console::ConsoleAppender,
+        rolling_file::{
+            policy::compound::{
+                roll::fixed_window::FixedWindowRoller, trigger::size::SizeTrigger, CompoundPolicy,
+            },
+            RollingFileAppender,
+        },
+    },
     config::{Appender, Config, Root},
     encode::pattern::PatternEncoder,
 };
@@ -11,12 +19,18 @@ fn main() -> Result<(), SetLoggerError> {
     let p = PatternEncoder::new("{d(%H:%M:%S)} - {l} - {m}\n");
     let stderr = ConsoleAppender::builder().encoder(Box::new(p)).build();
 
-    let logfile = FileAppender::builder()
+    let trigger = SizeTrigger::new(10 * 1024 * 1024); // 10 MB
+
+    let roller = FixedWindowRoller::builder()
+        .build("logs.{}.log", 10)
+        .unwrap();
+    let policy = CompoundPolicy::new(Box::new(trigger), Box::new(roller));
+    let logfile = RollingFileAppender::builder()
         .encoder(Box::new(PatternEncoder::new("{d} - {l} - {m}\n")))
-        .build(file_path)
+        .build(file_path, Box::new(policy))
         .unwrap();
 
-    let level = LevelFilter::Trace; // Trace < Debug < Info < Warn < Error
+    let level = LevelFilter::Info; // Trace < Debug < Info < Warn < Error
     let config = Config::builder()
         .appender(Appender::builder().build("logfile", Box::new(logfile)))
         .appender(Appender::builder().build("stderr", Box::new(stderr)))
