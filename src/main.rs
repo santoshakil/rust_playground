db88fb31e809d235bebc81e8d952339688d49552
Santo Shakil <shakil.mezbah@gmail.com>
2024-01-09 23:22:31 +0600

    local ip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::net::IpAddr;
+use std::net::UdpSocket;
+
+fn get_local_ip() -> Option<IpAddr> {
+    let socket = UdpSocket::bind("0.0.0.0:0").ok()?;
+    socket.connect("8.8.8.8:80").ok()?;
+    socket.local_addr().ok().map(|addr| addr.ip())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Some(local_ip) = get_local_ip() {
+        println!("Local IP address: {}", local_ip);
+    } else {
+        println!("Failed to retrieve local IP address");
+    }
 }
