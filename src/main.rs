0e869060813419bf860ebc6a1ec667ea399190e1
Santo Shakil <shakil.mezbah@gmail.com>
2024-09-03 16:57:17 +0600

    ..

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
-use fs_extra::dir::{self, CopyOptions};
-use std::env;
-use std::fs;
-
 fn main() {
-    if env::args().count() < 3 {
+    if std::env::args().count() < 3 {
         println!("Usage: <src> <dst>");
         std::process::exit(1);
     }
 
-    let src = env::args().nth(1).unwrap();
-    let dst = env::args().nth(2).unwrap();
+    let src = std::env::args().nth(1).unwrap();
+    let dst = std::env::args().nth(2).unwrap();
 
     // Remove the destination directory if it exists
     if std::path::Path::new(&dst).exists() {
-        if let Err(err) = fs::remove_dir_all(&dst) {
-            eprintln!("Failed to remove existing directory {}: {}", dst, err);
-            return;
+        if let Err(err) = std::fs::remove_dir_all(&dst) {
+            eprintln!("Error: {err}");
         }
     }
 
-    let config = CopyOptions::new().content_only(true).overwrite(true);
+    let config = fs_extra::dir::CopyOptions::new()
+        .content_only(true)
+        .overwrite(true);
 
-    if let Err(err) = dir::copy(&src, &dst, &config) {
+    if let Err(err) = fs_extra::dir::copy(&src, &dst, &config) {
         eprintln!("Error: {err}");
     }
 }
