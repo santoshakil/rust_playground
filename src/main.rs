99f131b22ef71fbeedc5f8a5fc9d59f7f12cbbe3
Santo Shakil <shakil.mezbah@gmail.com>
2024-09-26 16:40:22 +0600

    fsevent

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate fsevent;
+
+use std::sync::mpsc::channel;
+use std::thread;
+
+#[cfg(not(target_os = "macos"))]
+fn main() {}
+
+#[cfg(target_os = "macos")]
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() < 2 {
+        println!("Usage: cargo run -- <path>");
+        return;
+    }
+
+    let path = args[1].clone();
+
+    let (sender, receiver) = channel();
+
+    let _t = thread::spawn(move || {
+        let fsevent = fsevent::FsEvent::new(vec![path]);
+        fsevent.observe(sender);
+    });
+
+    loop {
+        let val = receiver.recv();
+        // ignore .DS_Store
+        if let Ok(val) = val {
+            if val.path.ends_with(".DS_Store") {
+                continue;
+            }
+            println!("{:?}", val);
+        }
+    }
 }
